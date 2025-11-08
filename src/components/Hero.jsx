b72fd35fd041240ef606edef4f442b4ebb02a349
Veritas <ochigbogodswill868@gmail.com>
2025-11-07 16:22:46 -0800

    background removed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import heroBg from "../assets/elanza2.jpeg"; 
+// import heroBg from "../assets/elanza2.jpeg"; 
 
 const Hero = () => {
   return (
